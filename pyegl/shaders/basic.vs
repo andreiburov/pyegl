bb18dca72b42b5ed125fef73fdb5de2575c0d356
Andrei Burov <aburov.post@gmail.com>
2021-06-01 23:07:15 +0200

    Cleanup. pyegl_test.py update.

--- pyegl/shaders/basic.vs
+++ pyegl/shaders/basic.vs
@@ -28,7 +28,6 @@ void main()
 {
   vec4 pos = vec4(in_position.xyz, 1.0);
 
-  //outData.position = (pos.xyz - mesh_normalization.xyz) * (1.0 / mesh_normalization.w);
   outData.position = pos.xyz;
   outData.normal = in_normal.xyz;
   outData.color = in_color;
@@ -39,27 +38,3 @@ void main()
   pos = modelview * vec4(outData.position, 1.0);
   gl_Position = projection * pos;
 }
-
-
-
-
-/*
-https://www.khronos.org/opengl/wiki/Interface_Block_(GLSL)
-
-Vertex Shaders have the following built-in input variables.
-
-in int gl_VertexID;
-in int gl_InstanceID;
-in int gl_DrawID; // Requires GLSL 4.60 or ARB_shader_draw_parameters
-in int gl_BaseVertex; // Requires GLSL 4.60 or ARB_shader_draw_parameters
-in int gl_BaseInstance; // Requires GLSL 4.60 or ARB_shader_draw_parameters
-
-Vertex Shaders have the following predefined outputs.
-
-out gl_PerVertex
-{
-  vec4 gl_Position;
-  float gl_PointSize;
-  float gl_ClipDistance[];
-};
-*/
--- pyegl_test.py
+++ pyegl_test.py
@@ -2,40 +2,53 @@
 import torch
 import pyegl
 import trimesh
+import imageio
 
-
-mesh = trimesh.load('data/bunny_col.obj', process=False)
+mesh = trimesh.load('data/bunny.obj', process=False)
 mesh.apply_translation(-mesh.centroid).apply_scale(1./mesh.extents)
 n_vertices = mesh.vertices.shape[0]
 n_faces = mesh.faces.shape[0]
 vertices = torch.tensor(mesh.vertices, dtype=torch.float32)
 normals = torch.tensor(mesh.vertex_normals.copy(), dtype=torch.float32)
-colors = torch.tensor(mesh.visual.to_color().vertex_colors / 255.0, dtype=torch.float32)
+# colors = torch.tensor(mesh.visual.to_color().vertex_colors / 255.0, dtype=torch.float32)
+colors = torch.ones((n_vertices, 4), dtype=torch.float32)
 uv = torch.tensor(mesh.visual.uv, dtype=torch.float32)
 mask = torch.ones((n_vertices, 1), dtype=torch.float32)
 vertices_data = torch.cat((vertices, normals, colors, uv, mask), dim=-1).cuda()
 faces = torch.tensor(mesh.faces, dtype=torch.int64)
-fx, fy, cx, cy, near, far = 4.14423, 4.27728, 0.5, 0.5, 0.1, 10.0
+
+
+fx, fy, cx, cy, near, far = 1000, 1000, 256, 256, 0.01, 100.0
 intrinsics = [fx, fy, cx, cy, near, far]
-pose = [1., 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, -10, 0, 0, 0, 1]
+pose = [1., 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 3, 0, 0, 0, 1]
 width, height = 512, 512
 
-pyegl.init(width, height)
+
+#pyegl.init(width, height, ['CONSTANT_SHADING'])
+pyegl.init(width, height, ['PHONG_SHADING'])
+#pyegl.init(width, height, ['TEXTURE_SHADING'])
+#pyegl.attach_texture('data/bunny-atlas.jpg')
 maps = pyegl.forward(intrinsics, pose, vertices_data, n_vertices, faces, n_faces)
 
-import matplotlib.pyplot as plt
-plt.figure(0)
-plt.imshow(maps[0].flip([0]).cpu().numpy())
-plt.figure(1)
-plt.imshow(maps[1].flip([0]).cpu().numpy()[..., 0:3])
-plt.figure(2)
-plt.imshow(maps[2].flip([0]).cpu().numpy()[..., 0:3])
-plt.figure(3)
-plt.imshow(torch.cat((maps[3].flip([0]), torch.zeros((height, width, 1), dtype=torch.float32).cuda()), dim=-1).cpu().numpy())
-plt.figure(4)
-plt.imshow(maps[4].flip([0]).cpu().numpy())
-plt.figure(5)
-plt.imshow((maps[5].flip([0])[..., 0:3]/maps[5].max()).cpu().numpy())
-plt.show()
+color = maps[0].flip([0]).cpu()
+color = torch.where(color[..., 0:3] == -1, torch.zeros_like(color[..., 0:3]), color[..., 0:3]).numpy()
+imageio.imwrite('results/color.png', color)
+
+positions = maps[1].flip([0]).cpu().numpy()[..., 0:3]
+imageio.imwrite('results/positions.png', positions)
+
+normals = maps[2].flip([0]).cpu().numpy()[..., 0:3]
+imageio.imwrite('results/normals.png', normals)
+
+uv = maps[3].flip([0]).cpu()
+uv = torch.where(uv == -1, torch.zeros_like(uv), uv)
+uv = torch.cat((uv, torch.zeros((height, width, 1), dtype=torch.float32)), dim=-1).numpy()
+imageio.imwrite('results/uv.png', uv)
+
+barycentrics = maps[4].flip([0]).cpu().numpy()
+imageio.imwrite('results/barycentrics.png', barycentrics)
+
+vertex_ids = (maps[5].flip([0])[..., 0:3]/maps[5].max()).cpu().numpy()
+imageio.imwrite('results/vertex_ids.png', vertex_ids)
 
 pyegl.terminate()
